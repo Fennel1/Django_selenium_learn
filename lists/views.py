9c7b999202cd00e17dfd2f04c8ae60cb3018130e
Fennel1 <627235787@qq.com>
2021-03-25 21:31:45 +0800

    3.25-21.30

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,9 @@
-
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page():
-    pass
-    # home_page = None
+
+def home_page(request):
+    # pass
+    # rerturn HttpResponse()
+    # return HttpResponse('<html>')
+    return HttpResponse('<html><title>To-Do lists</title></html>')
